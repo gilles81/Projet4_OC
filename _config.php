0a06ef0a014ddc50d0fbac73dad0daf16a5b5dae
Gilles Sabathe <gilles.sabathe@free.fr>
2018-04-06 15:19:04 -0400

    addition of autoload method : start() and autoload();

--- _config.php
+++ _config.php
@@ -6,23 +6,47 @@
 ini_set('display_errors', 'on');
 error_reporting(E_ALL);
 
+class MyAutoload
+{
+    public static function start()
+    {
+        spl_autoload(array(__CLASS,'autoload'));
+        $root= $_SERVER['DOCUMENT_ROOT'];
+        $host= $_SERVER['HTTP_HOST'];
 
-$root= $_SERVER['DOCUMENT_ROOT'];
-$host= $_SERVER['HTTP_HOST'];
+        /*
+         *
+         */
+        define('HOST','http//' . $host . '/P4_Test/');
+        define('ROOT',$root . 'P4_Test/');
+        /*
+         * Constant for absolute link
+         */
+        define('CONTROLLER', ROOT .'controller/');
+        define('VIEW',ROOT . 'view/');
+        define('MODEL','http'ROOT . 'model/');
+        define('LIB', ROOT .'lib/')
 
-/*
- *
- */
-define('HOST','http//' . $host . '/P4_Test/');
-define('ROOT',$root . 'P4_Test/');
-/*
- * Constant for absolute link
- */
-define('CONTROLLER', ROOT .'controller/');
-define('VIEW',ROOT . 'view/');
-define('MODEL','http'ROOT . 'model/');
-define('LIB', ROOT .'lib/')
+        define('ASSETS','http' . $host . 'assets/');
 
-define('ASSETS','http' . $host . 'assets/');
+    }
+
+    Public static function autoload($class)
+    {
+        if(file_exists(MODEL.$class.'.php'))
+        {
+            include_once(MODEL.$class.'.php');
+        }elseif (file_exists(CONTROLLER.$class.'.php'))
+        {
+            include_once(CONTROLLER.$class.'.php');
+        }elseif (file_exists(LIB.$class.'.php'))
+        {
+            include_once(LIB.$class.'.php');
+        };
+    }
+}
+
+
+
 
 
