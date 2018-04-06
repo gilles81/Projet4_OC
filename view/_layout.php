a74b23ccfa1b06eb71ab6171e8e499a3b6c651e6
Gilles Sabathe <gilles.sabathe@free.fr>
2018-04-06 10:10:19 -0400

    add of _layout.php

---
+++ view/_layout.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title> <?= $title ?> </title>
+
+        <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+        <!-- Feuille de style-->
+        <link rel="stylesheet" href="Style.css" />
+
+    </head>
+
+<body>
+        <!-- Header -->
+        <header>
+            <div class = "logo" > P4 </div>
+			<div class = "titre" > Billet simple pour l'Alaska test 1 </div>
+        </header>
+        <!-- page-->
+
+		 <?= $content ?>
+
+
+        <!-- footer -->
+        <footer>
+			<p> ceci est un test </p>
+        </footer>
+   </body>
+</html>
