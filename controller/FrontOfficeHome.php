ca3e2b10181ed37fd93238337fc25b4c8d7556b0
Gilles Sabathe <gilles.sabathe@free.fr>
2018-04-06 10:20:39 -0400

    add of FrontOfficeHome.php file in controller directory

---
+++ controller/FrontOfficeHome.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Class Post
+ *
+ * used to show the Post on home page
+ */
+class FrontOfficeHome
+{
+    public function showPosts()
+    {
+/*
+ * Todo
+ */
+    }
+
+    public function showPost()
+    {
+/*
+ * Todo
+ */
+    }
+
+}
