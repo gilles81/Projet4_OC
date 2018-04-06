d8e76c9c162a523f913ac7a798be6ee95a9480e1
Gilles Sabathe <gilles.sabathe@free.fr>
2018-04-06 10:17:40 -0400

    add of View.php file in lib directory

---
+++ lib/View.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *
+ * Class View
+ *
+ *
+ */
+
+class View {
+
+    private $template;
+
+    public function __construct($template)
+    {
+        $this -> template =$template;
+    }
+
+    public function build()
+    {
+        $template = $this-> template;
+        ob_start(VIEW.$template. '.php');
+        $content = ob_get_clean();
+        include(VIEW.'_gabarit.php');
+
+    }
+
+
+}
