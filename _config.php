19b46ebefb56ca9f577cfab0e69c994db7f6fdd9
Gilles Sabathe <gilles.sabathe@free.fr>
2018-04-08 23:49:38 -0400

    syntax correction

--- _config.php
+++ _config.php
@@ -10,39 +10,47 @@ class MyAutoload
 {
     public static function start()
     {
-        spl_autoload(array(__CLASS,'autoload'));
+        spl_autoload_register(array(__CLASS__,'autoload'));
+
         $root= $_SERVER['DOCUMENT_ROOT'];
         $host= $_SERVER['HTTP_HOST'];
 
-        /*
+        /**
          *
          */
         define('HOST','http//' . $host . '/Projet4/');
-        define('ROOT',$root . 'Projet4/');
-        /*
+        define('ROOT',$root .'/Projet4/');
+        /**
          * Constant for absolute link
          */
-        define('CONTROLLER', ROOT .'controller/');
+        define('CONTROLLER', ROOT.'controller/');
         define('VIEW',ROOT . 'view/');
         define('MODEL',ROOT . 'model/');
         define('LIB', ROOT .'lib/');
 
         define('ASSETS','http' . $host . 'assets/');
 
-    }
 
+
+
+    }
     Public static function autoload($class)
     {
-        if(file_exists(MODEL.$class.'.php'))
-        {
+
+        echo LIB.$class.'.php';
+
+        if (file_exists(MODEL.$class.'.php'))
+        {  echo '1';
             include_once(MODEL.$class.'.php');
-        }elseif (file_exists(CONTROLLER.$class.'.php'))
-        {
+        } elseif (file_exists (CONTROLLER.$class.'.php'))
+        {  echo '2';
             include_once(CONTROLLER.$class.'.php');
-        }elseif (file_exists(LIB.$class.'.php'))
+        } elseif (file_exists(LIB.$class.'.php'))
         {
             include_once(LIB.$class.'.php');
+
         };
+
     }
 }
 
