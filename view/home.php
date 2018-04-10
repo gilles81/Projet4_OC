75848608379073045763fb4ccbbf7c54dd4fdb51
Gilles Sabathe <gilles.sabathe@free.fr>
2018-04-10 15:14:16 -0400

    view correction for display the post - This view version  is made for debug -

--- view/home.php
+++ view/home.php
@@ -1,15 +1,14 @@
 
-
-<?php foreach($posts as $post):?>
+<?php foreach($chapters as $post):
+    ?>
     <div class="post">
         <h2><?php echo $post->getTitle();?></h2>
-        <div class="postContent" ><?php echo $post->getComment();?></div>
+        <div class="postContent" ><?php echo $post->getContent();?></div>
         <button>
             <a class ="commentLink" href="">Commentaire</a>
         </button>
     </div>
 <?php endforeach;?>
-
 
 
 
