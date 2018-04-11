6b7c3926293acad7d4483f5bdc09994a8d2bff66
Gilles Sabathe <gilles.sabathe@free.fr>
2018-04-11 09:36:25 -0400

    Deletion of conditional page selection . Are made now by a routeur class

--- index.php
+++ index.php
@@ -12,13 +12,5 @@ MyAutoload::start();
 
 $request = $_GET['r'];
 
-if ($request == "home.html")
-{
-    include_once(CONTROLLER.'PostController.php');
-}else{
-    echo 'Erreur 404 - La page ' . $request . ' est inaccessible';
-
-}
-
 $routeur = new Routeur($request);
 $routeur ->findController();
