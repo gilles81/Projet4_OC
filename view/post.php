99006024c0948621208c20d59aac92f6aba51973
Gilles Sabathe <gilles.sabathe@free.fr>
2018-04-11 16:55:49 -0400

    bootstrap theme integration

---
+++ view/post.php
@@ -0,0 +1,33 @@
+
+    <!-- Post Content -->
+    <article>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <h3><?php echo $chapter->getTitle();?> </h3>
+              <p><?php echo $chapter->getContent();?> </p>
+          </div>
+        </div>
+
+      </div>
+    </article>
+
+    <hr>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <h3>Commentaires</h3>
+          <div class="comment">
+              <?php echo 'To do';?>
+          </div>
+
+
+        </div>
+      </div>
+
+    </div>
+
+
+    <hr>
+
