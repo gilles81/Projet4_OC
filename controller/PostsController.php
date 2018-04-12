52157cefbfac4fcaee4da53f1811715def3ad775
Gilles Sabathe <gilles.sabathe@free.fr>
2018-04-12 13:51:27 -0400

    Controller creation with method : -showPosts();

---
+++ controller/PostsController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Class FrontOfficeHome
+ *
+ * used to show the Post.php on home page
+ */
+class PostsController
+{
+    public function showPosts()
+        {
+            /*get all posts in database*/
+            $manager = new PostManager();
+            $chapters= $manager->findAll();
+
+            $myView = new View('home');
+            $myView->build(array( 'chapters'=> $chapters));
+    }
+
+
+
+
+}
