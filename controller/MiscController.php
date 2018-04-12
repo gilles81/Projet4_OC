1f7acd11c1cdfda2765e57c60522b7d8c4020202
Gilles Sabathe <gilles.sabathe@free.fr>
2018-04-12 13:50:04 -0400

    Controller creation for Miscellanious services : showAbout() showContact()

---
+++ controller/MiscController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Class FrontOfficeHome
+ *
+ * used to show the Post.php on home page
+ */
+class MiscController
+{
+
+    public function showAbout()
+    {
+        /**
+         * Todo
+         *
+         */
+
+
+        $myView = new View('about');
+        $myView->build(array ());
+
+    }
+
+    public function showContact()
+    {
+        /**
+         * Todo
+         *
+         */
+
+        $myView = new View('contact');
+        $myView->build(array ());
+
+    }
+
+}
