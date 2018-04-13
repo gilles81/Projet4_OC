201edb2cf6eb930afd65323feb04dad4bd117704
Gilles Sabathe <gilles.sabathe@free.fr>
2018-04-13 10:39:50 -0400

    route tab correction with a corresponding controller.

--- view/_layout.php
+++ view/_layout.php
@@ -7,6 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <!-- Tinny Mce initialisation -->
+    <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+    <script>tinymce.init({ selector:'.admintext' });</script>
+    </head>
 
     <title>Billet simple pour l'Alaska - Jean Forteroche </title>
 
@@ -21,6 +25,8 @@
     <!-- Custom styles for this template -->
     <link href="./assets/css/clean-blog.min.css" rel="stylesheet">
 
+    <link href="assets/Style.css" rel="stylesheet">
+
 </head>
 
 <body>
@@ -28,7 +34,7 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="home.html">une histoire de Jean Forteroche</a>
+        <a class="navbar-brand" href="home.html">Une histoire de Jean Forteroche</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fa fa-bars"></i>
@@ -41,9 +47,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="about.html">A propos</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="post.html">Administartion</a>
-                </li>
+
                 <li class="nav-item">
                     <a class="nav-link" href="contact.html">Contact</a>
                 </li>
