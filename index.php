a6464d4abbe15acc3e6f0abda11757d218cad9ec
Gilles Sabathe <gilles.sabathe@free.fr>
2018-04-13 15:47:31 -0400

    implemetation of twig

--- index.php
+++ index.php
@@ -8,9 +8,22 @@
  */
 
 include_once('_config.php');
+/* initialisation des fichiers TWIG */
+/*require_once 'vendor/twig/twig/lib/Twig/autoloader.php';*/
+require_once 'vendor/autoload.php';
+/** Autoload */
 MyAutoload::start();
 
 $request = $_GET['r'];
+
+/**
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/view');
+$twig = new Twig_Environment($loader , [
+    'cache' => false //__DIR__ .'/tmp'
+]);
+ **/
+
+/*echo $twig->render('test.twig');*/
 
 $routeur = new Routeur($request);
 $routeur ->findController();
