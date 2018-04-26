550a82b5bd85c6d03b5eb2d2fd26fb93d6e2b969
Gilles Sabathe <gilles.sabathe@free.fr>
2018-04-26 14:52:42 -0400

    class renamed topic intead of comment

--- model/entities/Topic.php
+++ model/entities/Topic.php
@@ -9,7 +9,7 @@
  */
 
 
-class Comment
+class Topic
 {
     private $commentId;
     private $postId;
@@ -17,79 +17,123 @@ class Comment
     private $creationDate;
     private $moderationDate;
     private $commentContent;
+    private $AnswerId;
 
-     /**
+    /**
+     * @return mixed
+     */
+    public function getAnswerId()
+    {
+        return $this->AnswerId;
+    }
+
+    /**
+     * @param mixed $AnswerId
+     */
+    public function setAnswerId($AnswerId)
+    {
+        $this->AnswerId = $AnswerId;
+    }
+    /**
      * @return mixed
      */
     public function getCommentId()
     {
         return $this->commentId;
-    }/**
+    }
+
+    /**
      * @param mixed $commentId
      */
     public function setCommentId($commentId)
     {
         $this->commentId = $commentId;
-    }/**
+    }
+
+    /**
      * @return mixed
      */
     public function getPostId()
     {
         return $this->postId;
-    }/**
+    }
+
+    /**
      * @param mixed $postId
      */
     public function setPostId($postId)
     {
         $this->postId = $postId;
-    }/**
+    }
+
+    /**
      * @return mixed
      */
     public function getAuthor()
     {
         return $this->Author;
-    }/**
+    }
+
+    /**
      * @param mixed $Author
      */
     public function setAuthor($Author)
     {
         $this->Author = $Author;
-    }/**
+    }
+
+    /**
      * @return mixed
      */
     public function getCreationDate()
     {
         return $this->creationDate;
-    }/**
+    }
+
+    /**
      * @param mixed $creationDate
      */
     public function setCreationDate($creationDate)
     {
         $this->creationDate = $creationDate;
-    }/**
+    }
+
+    /**
      * @return mixed
      */
     public function getModerationDate()
     {
         return $this->moderationDate;
-    }/**
+    }
+
+    /**
      * @param mixed $moderationDate
      */
     public function setModerationDate($moderationDate)
     {
         $this->moderationDate = $moderationDate;
-    }/**
+    }
+
+    /**
      * @return mixed
      */
     public function getCommentContent()
     {
         return $this->commentContent;
-    }/**
+    }
+
+    /**
      * @param mixed $commentContent
      */
     public function setCommentContent($commentContent)
     {
         $this->commentContent = $commentContent;
     }
+
+    /**
+     * @return mixed
+     */
+
+
 
 }
