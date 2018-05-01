b0a033c769106c42d136bb0cfe526e96803bcad5
Gilles Sabathe <gilles.sabathe@free.fr>
2018-05-01 13:42:19 -0400

    add in method bddAssign a contant in call of PDO for bddname

--- model/BackManager.php
+++ model/BackManager.php
@@ -8,10 +8,9 @@
 class BackManager extends lib
 {
     private  $bdd;
-
     Public function bddAssign() {
         try {
-            return $this->bdd = new PDO("mysql:host=localhost;dbname=".BLOGNAME.";charset=utf8", "root", "");
+            return $this->bdd = new PDO("mysql:host=".BDDDIR.";dbname=".BLOGNAME.";charset=utf8", "root", "");
         }catch(Execption $e){
             die('Erreur : '.$e->getMessage());
         }
