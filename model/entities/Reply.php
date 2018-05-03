3417bf8767cd6a8849b357e3c935c974c38836d0
Gilles Sabathe <gilles.sabathe@free.fr>
2018-05-02 23:03:42 -0400

    add of class Reply

---
+++ model/entities/Reply.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Class Reply.php
+ *
+ * Define a Post.php
+ *
+ *
+ */
+
+
+class Reply
+{
+    private $id;
+
+
+    private $answer;
+    private $answerId;
+    private $author;
+
+    private $moderation;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAnswer()
+    {
+        return $this->answer;
+    }
+
+    /**
+     * @param mixed $answer
+     */
+    public function setAnswer($answer)
+    {
+        $this->answer = $answer;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAnswerId()
+    {
+        return $this->answerId;
+    }
+
+    /**
+     * @param mixed $answerId
+     */
+    public function setAnswerId($answerId)
+    {
+        $this->answerId = $answerId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModeration()
+    {
+        return $this->moderation;
+    }
+
+    /**
+     * @param mixed $moderation
+     */
+    public function setModeration($moderation)
+    {
+        $this->moderation = $moderation;
+    }
+}
