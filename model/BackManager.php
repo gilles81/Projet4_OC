7b2704f8e9bc20e8028845ac24c7c127ac58a8a2
Gilles Sabathe <gilles.sabathe@free.fr>
2018-05-03 10:22:56 -0400

    add constant for cnx

--- model/BackManager.php
+++ model/BackManager.php
@@ -10,7 +10,7 @@ class BackManager extends lib
     private  $bdd;
     Public function bddAssign() {
         try {
-            return $this->bdd = new PDO("mysql:host=".BDDDIR.";dbname=".BLOGNAME.";charset=utf8", "root", "");
+            return $this->bdd = new PDO("mysql:host=".BDDDIR.";dbname=".BLOGNAME.";charset=utf8", USERNAME, PSWDB);
         }catch(Exception $e){
             die('Erreur : '.$e->getMessage());
         }
