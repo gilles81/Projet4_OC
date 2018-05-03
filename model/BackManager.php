4b7d5e18b613accec8b2ab0b14bff5d9f75e9357
Gilles Sabathe <gilles.sabathe@free.fr>
2018-05-03 15:11:52 -0400

    link correction for pdo

--- model/BackManager.php
+++ model/BackManager.php
@@ -10,6 +10,7 @@ class BackManager extends lib
     private  $bdd;
     Public function bddAssign() {
         try {
+            //return $this->bdd = new PDO("mysql:host=".BDDDIR.";dbname=".BLOGNAME.";charset=utf8", USERNAME, PSWDB);
             return $this->bdd = new PDO("mysql:host=".BDDDIR.";dbname=".BLOGNAME.";charset=utf8", USERNAME, PSWDB);
         }catch(Exception $e){
             die('Erreur : '.$e->getMessage());
