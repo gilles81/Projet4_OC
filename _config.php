d281737a66d39999ff466a76d862468f1b53d16e
Gilles Sabathe <gilles.sabathe@free.fr>
2018-05-03 10:21:45 -0400

    add constant

--- _config.php
+++ _config.php
@@ -35,6 +35,16 @@ class MyAutoload
 
         define('BLOGNAME','blogecrivain');
         define('BDDDIR','localhost');
+        define('USERNAME','root');
+        define('PSWDB','');
+/**
+        define('BLOGNAME','db736607164');
+        define('BDDDIR','/db736607164.db.1and1.com');
+        define('USERNAME','dbo736607164');
+        define('PSWDB','JForteroche1234.');
+      **/
+
+
     }
     Public static function autoload($class)
     {
