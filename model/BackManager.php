a5441d593602e075178884de9805f9e95275b24c
Gilles Sabathe <gilles.sabathe@free.fr>
2018-05-08 23:25:30 -0400

    comments Modification

--- model/BackManager.php
+++ model/BackManager.php
@@ -1,16 +1,29 @@
 <?php
 /**
- * class commentManager
+ * class BackManager
  *
- *  Get comment
+ *  define common function for other manager
+ *
+ *
  */
 
 class BackManager extends lib
 {
     private  $bdd;
+
+
+    /**
+     *
+     * Public function bddAssign() {
+     *
+     * Connection to BDD
+     *
+     * @return PDO
+     *
+     */
+
     Public function bddAssign() {
         try {
-            //return $this->bdd = new PDO("mysql:host=".BDDDIR.";dbname=".BLOGNAME.";charset=utf8", USERNAME, PSWDB);
             return $this->bdd = new PDO("mysql:host=".BDDDIR.";dbname=".BLOGNAME.";charset=utf8", USERNAME, PSWDB);
         }catch(Exception $e){
             die('Erreur : '.$e->getMessage());
